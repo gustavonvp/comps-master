c0864aca4e0cb3265ca0fe8bc8ffb6e17c77799d
Gustavonvp <gustavonvp@gmail.com>
2021-12-18 15:51:31 -0300

    commit element update

--- src/elements/elements.module.ts
+++ src/elements/elements.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from '../app/shared/shared.module';
 import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
